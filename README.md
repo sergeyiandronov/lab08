80a6caf8d8a68bf145611ebebaadf4f8a4693810
sergeyiandronov <sergeyiandronov@gmail.com>
2018-05-14 15:46:17 +0300

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/sergeyiandronov/lab06.svg?branch=master)](https://travis-ci.org/sergeyiandronov/lab06)
+[![Build Status](https://travis-ci.org/sergeyiandronov/lab07.svg?branch=master)](https://travis-ci.org/sergeyiandronov/lab07)
   
 
 
--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,12 @@
+/** Вывод строки */
 #include <string>
 #include <fstream>
 #include <iostream>
-
+/** @brief Вывод текста в команндную строку
+    @param ссылка на выводимую строку, поток вывода.
+    */
 void print(const std::string& text, std::ostream& out = std::cout);
+/** @brief Вывод текста в команндную строку
+    @param ссылка на выводимую строку, файловый поток вывода.
+    */
 void print(const std::string& text, std::ofstream& out);
