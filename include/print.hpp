0222f4c4c9b82187212921b145598ec442e81d82
sergeyiandronov <sergeyiandronov@gmail.com>
2018-05-14 16:50:26 +0300

    hsgcv

--- include/print.hpp
+++ include/print.hpp
@@ -3,10 +3,13 @@
 #include <fstream>
 #include <iostream>
 /** @brief Вывод текста в команндную строку
-    @param ссылка на выводимую строку, поток вывода.
+    @param text ссылка на выводимую строку
+    @param out  поток вывода
     */
 void print(const std::string& text, std::ostream& out = std::cout);
 /** @brief Вывод текста в команндную строку
-    @param ссылка на выводимую строку, файловый поток вывода.
+    @param text ссылка на выводимую строку
+    @param out файловый поток вывода
     */
 void print(const std::string& text, std::ofstream& out);
+
